fc1a131466ca07be47d133cb56fdd3cfabb00b3b
Richard Bobo <richard.bobo@gmail.com>
2020-03-01 23:25:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,19 +1,16 @@
 def first_challenge
+  
    epic_tragedy = {
      
      :montague  => {
+       
        :patriarch => { :name => "Lord Motague", :age => "53"},
       :matriarch => { :name => "Lady Montague", :age => "54"},
       :hero => { :name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends  => [
         
-       { :name => "Benvolio", :age => "17", :attitude => "worried"
-        },
-        
-        { :name => "Mercutio", :age => "18", :attitude => "hot-headed"
-        }
-        
-        
+       { :name => "Benvolio", :age => "17", :attitude => "worried"},
+        { :name => "Mercutio", :age => "18", :attitude => "hot-headed"}
         
         ]
         },

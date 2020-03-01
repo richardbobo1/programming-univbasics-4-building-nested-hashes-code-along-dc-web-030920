a1e43f80a4b433dd1052eb3a2d7686d365a6081e
Richard Bobo <richard.bobo@gmail.com>
2020-03-01 23:13:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -10,9 +10,9 @@ def first_challenge
         
      :capulet => {
        
-        :patriarch => {},
-        :matriarch  => {},
-        :heroine  => {},
+        :patriarch => { :name => "Lord Capulet", :age => "50"},
+        :matriarch  => { :name => "Lady Capulet", :age => "51"},
+        :heroine  => { :name => "Juliet", :age => "15", :status => "alive"},
         :heroine_friends => []
      }
      
